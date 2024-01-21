a76f5fdc46441188815165202df9bfb936ea2d2f
AnuragMaurya14 <a.maurya1404@gmail.com>
2024-01-21 16:12:12 +0530

    strings

---
+++ string.js
@@ -0,0 +1,20 @@
+let name = "Anurag"
+console.log(name.length)
+console.log(name[0])
+console.log(name[1])
+console.log(name[2])
+console.log(name[3])
+console.log(name[4])
+console.log(name[5])
+let fr = "Rahul"
+let sentence =(`${name} is a friend of ${fr}`)
+console.log(sentence)
+
+// program for converting givrn string in lower case
+let a = "HELLO"
+console.log(a.toLowerCase())
+
+// to slice the not requried part
+let str = "give me rs 1000"
+let b = Number.parseInt (str.slice(11))
+console.log(b)

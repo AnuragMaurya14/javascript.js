0fa09c4ce9c63623aa797cab4f986c87614cf2a6
AnuragMaurya14 <a.maurya1404@gmail.com>
2024-01-26 17:26:03 +0530

    functions

---
+++ functions_methods.js
@@ -0,0 +1,10 @@
+// for counting vowels in the string 
+function vowels(string){
+    let count =0;
+    for (const char of string){
+        if(char==="a"||char==="e"||char==="i"||char==="o"||char==="u"){
+        count++;
+    }
+}
+console.log(count)
+}

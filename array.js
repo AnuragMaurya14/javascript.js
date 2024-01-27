10570fd736337aba850089884f5f4aef48b29743
AnuragMaurya14 <a.maurya1404@gmail.com>
2024-01-27 15:14:28 +0530

    array methods

--- array.js
+++ array.js
@@ -1,10 +1,10 @@
 //for give marks of student in form of array find the average marks of class
-let marks=[23,94,56,95,43,65,78];
-let sum=0;
-for(let value of marks){
-    sum += value;
+let mark=[23,94,56,95,43,65,78];
+let add=0;
+for(let value of mark){
+    add += value;
 }
-let avg=sum/marks.length;
+let avg=add/mark.length;
 console.log(`avg marks of the class = ${avg}`)
 
 //to display the price of item after applying 10% of offer
--- functions_methods.js
+++ functions_methods.js
@@ -6,5 +6,31 @@ function vowels(string){
         count++;
     }
 }
-console.log(count)
+return count;
 }
+// for calculating square of no. in array using foreach array
+let squares = [23,34,45,56,7];
+squares.forEach((val)=>{
+    console.log(val**2)
+})
+//filter out the marks of student greater then 90 in array
+let marks = [87,93,64,99,86,100]
+let result=marks.filter((val)=>{
+    return val>90;
+})
+console.log(result)
+// calculating sum and factorial using reduce method
+let n = prompt("Enter a number");
+let array=[];
+for(let i=1;i<=n;i++){
+    array[i-1]=i;
+}
+console.log(array)
+let sum = array.reduce((res,cur)=>{
+    return res+cur;
+})
+console.log(sum);
+let fact=array.reduce((res,cur)=>{
+      return res*cur;
+})
+console.log(fact)

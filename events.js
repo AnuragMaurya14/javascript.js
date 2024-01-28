75ea77ac3c6007cec9cd5ab484635cd4b9df71ba
AnuragMaurya14 <a.maurya1404@gmail.com>
2024-01-28 12:06:15 +0530

    events

---
+++ events.js
@@ -0,0 +1,5 @@
+let button= document.createElement("button")
+button.innerText="click me!"
+button.style.backgroundColor="blue";
+button.style.color="white"
+document.querySelector("body").append(button)

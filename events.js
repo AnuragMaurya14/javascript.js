f4b36367ffdd76ba72904dddfbd79246d2409d30
AnuragMaurya14 <a.maurya1404@gmail.com>
2024-01-28 13:15:47 +0530

    use of events

--- events.js
+++ events.js
@@ -1,5 +1,23 @@
-let button= document.createElement("button")
-button.innerText="click me!"
-button.style.backgroundColor="blue";
-button.style.color="white"
-document.querySelector("body").append(button)
+// creating a toggle button to changes screen to light mode to dark mode & dark mode to light mode
+let mode = document.querySelector("#mode")
+let curr="light";
+mode.addEventListener("click",()=>{
+    if (curr==="light"){
+        curr="dark";
+        document.querySelector("body").style.backgroundColor="black"
+    }else{
+        curr="light";
+        document.querySelector("body").style.backgroundColor="white"
+    }
+    console.log(curr)
+})
+
+
+
+
+
+let div =document.querySelector("div")
+div.addEventListener("mouseover",()=>{
+    console.log("How are you?")
+    console.log(div)
+})
